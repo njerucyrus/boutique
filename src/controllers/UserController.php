cacb55c55b6283726d5814c408f9a2b92be29b94
njerucyrus <njerucyrus5@gmail.com>
2018-01-15 23:24:18 +0300

    bindValue

--- src/controllers/UserController.php
+++ src/controllers/UserController.php
@@ -38,7 +38,7 @@ class UserController implements CrudOps
                     ->prepare("INSERT INTO users(username, password, user_type)
                           VALUES (:username, :password, :user_type)");
                 $stmt->bindParam(":username", $data['username']);
-                $stmt->bindParam(":password", password_hash($data['password'], PASSWORD_BCRYPT));
+                $stmt->bindValue(":password", password_hash($data['password'], PASSWORD_BCRYPT));
                 $stmt->bindParam(":user_type", $data['user_type']);
                 $query = $stmt->execute();
                 if ($query) {
